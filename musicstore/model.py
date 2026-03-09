b049e96b1fec327b70fab08fd345121cd1c6cd37
imdarlings <dhinestroza880@soyudemedellin.edu.co>
2026-03-09 16:21:38 -0500

    clase transaccion

--- musicstore/model.py
+++ musicstore/model.py
@@ -1,3 +1,11 @@
 from datetime import datetime
 
 # TODO: Add code here
+
+class Transaction:
+    SELL:int = 1
+    SUPPLY:int = 2
+    def __init__(self,type: int, copies:int):
+        self.type = type
+        self.copies = copies
+        self.date = datetime.now()
